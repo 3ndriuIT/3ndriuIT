5a0e64081e4e7596e87ad2d8ccf6f3586d78df2a
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-08-15 21:23:06 +0200

    Add files via upload

---
+++ So-LoName.py
@@ -0,0 +1,14 @@
+# z podanej listy liczb policz te, które sa podzielne przez 4 i te ktore przez 5
+
+names = ['karolina', 'andzelina', 'wladyslaw', 'marek', 'konrad', 'mariusz', 'maks']
+shortest_name = None
+longest_name = None
+
+for name in names:
+    if shortest_name is None or len(name) < len(shortest_name):
+        shortest_name = name
+    if longest_name is None or len(name) > len(longest_name):
+        longest_name = name
+
+print(f'longest name is: {longest_name}')
+print(f'shortest name is: {shortest_name}')
