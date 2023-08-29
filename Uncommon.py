98628baec438d363ab8fc349930a2f7b37b72e35
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-08-29 19:09:58 +0200

    Add files via upload

---
+++ Uncommon.py
@@ -0,0 +1,6 @@
+# Uncommon letters from words
+
+# word1 = set(input('Podaj pierwsze słowo: '))
+# word2 = set(input('Podaj drugie słowo: '))
+# # rozdzielone = (word1.split(' ')) + (word2.split(' '))
+# print(word1 ^ word2)
