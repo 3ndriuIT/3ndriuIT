b1dc367e1b7ba9b82ac89c5520340199e5097ea7
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-09-10 12:13:46 +0200

    Add files via upload

---
+++ emailswoutdup.py.py
@@ -0,0 +1,9 @@
+# Zadanie 13.1.3
+#Odbierz od uzytkownika 10 adresow email i wyswietl je ponizej bez duplikatów
+
+emails = set()
+
+for _ in range(10):
+    emails.add(input('podaj adres email: '))
+
+print(emails)
