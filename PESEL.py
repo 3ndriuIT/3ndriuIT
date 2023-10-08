ed72033ed75239de2a1f079a3ea5047d486e59b0
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-10-08 10:58:57 +0200

    Add files via upload

---
+++ PESEL.py
@@ -0,0 +1,12 @@
+# Sprawdz czy pesel jest poprawny.
+# pesel = '92123006553'
+# p_check = '13791379131'
+#
+# correct = 0
+# for number, _ in enumerate(pesel):
+#     correct += int(pesel[number]) * int(p_check[number])
+#
+# if str(correct)[-1] == '0':
+#     print('pesel jest prawidłowy')
+# else:
+#     print('Pesel jest zły')
