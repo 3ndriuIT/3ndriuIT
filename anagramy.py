5bff436dc12d975ade559bc572951a190b88bfc7
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-10-08 11:15:01 +0200

    Update anagramy.py

--- anagramy.py
+++ anagramy.py
@@ -1,9 +1,9 @@
-# sprawdz czy słowa są anagramami!
-# 
-# word1 = input('Podaj słowo: ')
-# word2 = input('Podaj słowo: ')
-#
-# if sorted(word1) == sorted(word2):
-#     print('To są anagramy! ')
-# else:
-#     print('To nie są anagramy!')
+sprawdz czy słowa są anagramami!
+
+word1 = input('Podaj słowo: ')
+word2 = input('Podaj słowo: ')
+
+if sorted(word1) == sorted(word2):
+    print('To są anagramy! ')
+else:
+    print('To nie są anagramy!')
