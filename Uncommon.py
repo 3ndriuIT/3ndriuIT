7f481c113ead441c19947a7ca74428a27cd93c39
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-10-08 11:14:48 +0200

    Update Uncommon.py

--- Uncommon.py
+++ Uncommon.py
@@ -1,6 +1,5 @@
 # Uncommon letters from words
 
-# word1 = set(input('Podaj pierwsze słowo: '))
-# word2 = set(input('Podaj drugie słowo: '))
-# # rozdzielone = (word1.split(' ')) + (word2.split(' '))
-# print(word1 ^ word2)
+word1 = set(input('Podaj pierwsze słowo: '))
+word2 = set(input('Podaj drugie słowo: '))
+print(word1 ^ word2)
