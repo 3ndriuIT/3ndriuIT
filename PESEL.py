5d98cae7174ec64490ad94406c86802e7a757e3a
Andrzej <142082864+3ndriuIT@users.noreply.github.com>
2023-10-16 18:46:58 +0200

    Update PESEL.py
    Task upgraded > zip and shortest if statement :)

--- PESEL.py
+++ PESEL.py
@@ -1,13 +1,19 @@
-# Sprawdz czy pesel jest poprawny.
+# Check your identification number ! 
 
-pesel = '92123006553'
-p_check = '13791379131'
+pesel = input('Wpisz swoj nr pesel: ')
+check = '13791379131'
 
-correct = 0
-for number, _ in enumerate(pesel):
-    correct += int(pesel[number]) * int(p_check[number])
+control = 0
+# for number, _ in enumerate(pesel):
+#     correct += int(pesel[number]) * int(p_check[number])
+# BEZ ZIPPA !
 
-if str(correct)[-1] == '0':
-    print('pesel jest prawidłowy')
-else:
-    print('Pesel jest zły')
+for pesel_digit, check_digit in zip(pesel, check):
+    control += int(pesel_digit) * int(check_digit)
+
+# if str(control)[-1] == '0':
+#     print('pesel jest prawidłowy')
+# else:
+#     print('Pesel jest zły')
+
+print("pesel jest ok!") if str(control)[-1] == '0' else print('pesel jest zly!')
